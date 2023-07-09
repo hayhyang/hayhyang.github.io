5b9eb43d7aadceb2351b4ca5b85a03d267487a86
hay <hayhyang@gmail.com>
2023-07-09 21:51:29 +0900

    feat: 페이지 레이아웃

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/Home.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <h1>Blog</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
